29392db03dea9a538aa5c206fbf7645382d42bbd
gusrud8457 <fjqm7280@naver.com>
2022-09-03 21:34:44 +0900

    프로젝트 첫 커밋 변수와 상수 수업 코드

---
+++ JavaStudy_Kyeong/src/j02_변수와_상수/MaxMin.java
@@ -0,0 +1,14 @@
+package j02_변수와_상수;
+
+public class MaxMin { //class name은 첫	번째 문자를 대문자로 쓰기!
+
+	public static void main(String[] args) {
+		//상수는 모든 문자를 대문자로 쓰기!
+		final int MAX_NUMBER = 100;
+		final int MIN_NUMBER = 0;
+		
+		
+
+	}
+
+}
---
+++ JavaStudy_Kyeong/src/j02_변수와_상수/MyInfo.java
@@ -0,0 +1,36 @@
+package j02_변수와_상수;
+
+public class MyInfo {
+
+	public static void main(String[] args) {
+		//주석
+		/*
+		 * 
+		 */
+		
+		char a = '정';
+		char b = '현';
+		char c = '경';
+		int age = 27;
+		String address = "부산시 해운대구 재송동";
+		String phone = "010-3144-8457";
+		
+		
+		System.out.print("이름 : ");
+		System.out.print(a);
+		System.out.print(b);
+		System.out.println(c);
+		
+		System.out.print("나이 : ");
+		System.out.println(age);
+		
+		System.out.print("주소 : ");
+		System.out.println(address);
+		
+		System.out.print("번호 : ");
+		System.out.println(phone);
+		
+
+	}
+
+}
